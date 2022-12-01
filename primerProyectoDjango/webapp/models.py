e5d447e6cdca98bb36a0c2cd31a5747063b0b612
Jose Luis Llorente <joseluis.llorenteperales@gmail.com>
2022-12-01 10:10:00 +0100

    Models django

--- primerProyectoDjango/webapp/models.py
+++ primerProyectoDjango/webapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Persona(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellidos = models.CharField(max_length=60)
+    dni = models.CharField(max_length=9)
+    email = models.CharField(max_length=100)
